e21535318c01c7eaf4decf7f20b69ad12e962bfd
ovnrain <ovnrain@gmail.com>
2022-06-24 18:04:03 +0800

    update router name

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-import v1 from './router/v1';
+import v1Router from './router/v1';
 
 const PORT = parseInt(process.env.PORT || '', 10) || 3000;
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 app.disable('x-powered-by');
 app.set('trust proxy', true);
 
-app.use('/api/v1', v1);
+app.use('/api/v1', v1Router);
 
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}!`);

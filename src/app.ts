b53796d7828fded1b1d615e8fe79884721b3ded9
ovnrain <ovnrain@gmail.com>
2022-11-09 02:53:47 +0800

    refactor: split app

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import createError from 'http-errors';
+import v1Router from './router/v1/router';
+import { errorHandler } from './handlers';
+
+const app = express();
+
+app.disable('x-powered-by');
+app.set('trust proxy', true);
+
+app.use(express.static('public'));
+app.use('/api/v1', v1Router);
+
+app.use((_req, _res, next) => {
+  next(new createError.NotFound());
+});
+
+app.use(errorHandler);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,23 @@
 import http from 'http';
-import express from 'express';
-import createError from 'http-errors';
-import type { ListenError } from './types';
 import { normalizePort } from './utils';
-import v1Router from './router/v1/router';
-import { errorHandler } from './handlers';
+import app from './app';
+import type { ListenError } from './types';
 
 const PORT = normalizePort(process.env.PORT || '3000');
-const app = express();
-
-app.set('port', PORT);
-app.disable('x-powered-by');
-app.set('trust proxy', true);
-
-app.use(express.static('public'));
-app.use('/api/v1', v1Router);
-
-app.use((_req, _res, next) => {
-  next(new createError.NotFound());
-});
-
-app.use(errorHandler);
 
 const server = http.createServer(app);
 
 server.listen(PORT);
+server.on('error', onError);
+server.on('listening', onListening);
 
-server.on('error', (error: ListenError) => {
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : `port ${addr?.port}`;
+  console.log(`app listening on ${bind}!`);
+}
+
+function onError(error: ListenError) {
   if (error.syscall !== 'listen') {
     throw error;
   }
@@ -42,10 +33,4 @@ server.on('error', (error: ListenError) => {
   } else {
     throw error;
   }
-});
-
-server.on('listening', () => {
-  const addr = server.address();
-  const bind = typeof addr === 'string' ? 'pipe ' + addr : `port ${addr?.port}`;
-  console.log(`app listening on ${bind}!`);
-});
+}

fed4aa37b747602fd39cdab4ecebd5da124c4dfc
ovnrain <ovnrain@gmail.com>
2026-01-05 15:17:59 +0800

    chore: update development setup with Hono framework and adjust TypeScript configuration

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);

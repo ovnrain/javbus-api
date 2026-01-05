f10d6a23bff4382b8f43f372e11bafd54b84f1e3
ovnrain <ovnrain@gmail.com>
2026-01-05 16:03:01 +0800

    refactor: remove deprecated API files and clean up project structure

--- api/utils.ts
+++
@@ -1,7 +0,0 @@
-import { JAVBUS } from './constants.js';
-
-export const PAGE_REG = /^[1-9]\d*$/;
-
-export function formatImageUrl(url?: string) {
-  return url && !/^http/.test(url) ? `${JAVBUS}${url}` : url;
-}
